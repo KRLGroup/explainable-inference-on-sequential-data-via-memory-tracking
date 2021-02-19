17435718a829734954eb0e06d1af0c3e0e6b0c89
biagiomattialarosa <biagiomattialarosa@gmail.com>
2021-02-19 13:05:53 +0100

    moved flag initialization to init and improved function to concatenate flags and vectors

--- core/model.py
+++ core/model.py
@@ -6,22 +6,24 @@ from core.dnc.SimplifiedDNC import SimplifiedDNC
 class ClozeModel(nn.Module):
 
     def __init__(self, controller_config, memory_config, embedding_dim, embed_len ,
-                 dropout=0.2):
-        super(ClozeModel, self).__init__()
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        assert controller_config.input_size == embedding_dim
-        self.embeddings = nn.Embedding(embed_len, embedding_dim, padding_idx=0)
+                    dropout=0.2):
+            super(ClozeModel, self).__init__()
+            assert controller_config.input_size == embedding_dim
 
-        self._num_flags = 3
-        self.flag_story = torch.as_tensor([1, 0, 0], dtype=torch.float32).to(self.device)
-        self.flag_o1 = torch.tensor([0, 1, 0], dtype=torch.float32, device=self.device)
-        self.flag_o2 = torch.tensor([0, 0, 1], dtype=torch.float32, device=self.device)
+            # flags
+            self._num_flags = 3
+            self._word_size = embedding_dim+self._num_flags
+            self.flag_story = torch.nn.parameter.Parameter(torch.as_tensor([[[1, 0, 0]]], dtype=torch.float32),requires_grad=False)
+            self.flag_o1 = torch.nn.parameter.Parameter(torch.tensor([0, 1, 0], dtype=torch.float32),requires_grad=False)
+            self.flag_o2 = torch.nn.parameter.Parameter(torch.tensor([0, 0, 1], dtype=torch.float32),requires_grad=False)
+            self.query = torch.nn.parameter.Parameter(torch.zeros((1, 1, self._word_size)),requires_grad=False)
 
-        self._word_size = embedding_dim+self._num_flags
 
-        self.dnc = SimplifiedDNC(controller_config, memory_config, output_dim=memory_config.word_size,
-                                 clip_value=controller_config.clip_value, dropout=dropout)
-        self.fully_connected_layer = nn.Linear(memory_config.word_size*2, 2)
+            # layers
+            self.embeddings = nn.Embedding(embed_len, embedding_dim, padding_idx=0)
+            self.dnc = SimplifiedDNC(controller_config, memory_config, output_dim=memory_config.word_size,
+                                    clip_value=controller_config.clip_value, dropout=dropout)
+            self.fully_connected_layer = nn.Linear(memory_config.word_size*2, 2)
 
     def forward(self, story_batch, answers):
         option1_batch = answers[0]
@@ -34,32 +36,16 @@ class ClozeModel(nn.Module):
         option1_embeds = self.embeddings(option1_batch)
         option2_embeds = self.embeddings(option2_batch)
     
-        story_vector = torch.zeros((batch_size, len(story_embeds[0]), self._word_size)).to(self.device)
-        o1_vector = torch.zeros((batch_size, len(option1_embeds[0]), self._word_size)).to(self.device)
-        o2_vector = torch.zeros((batch_size, len(option2_embeds[0]), self._word_size)).to(self.device)
+        # add flags
+        story_vector = torch.cat([story_embeds,self.flag_story.repeat(batch_size,story_batch.shape[1],1)],2)
+        o1_vector =  torch.cat([option1_embeds,self.flag_o1.repeat(batch_size,option1_batch.shape[1],1)],2)
+        o2_vector = torch.cat([option2_embeds,self.flag_o2.repeat(batch_size,option2_batch.shape[1],1)],2)
     
-        for batch in range(len(story_embeds)):
-            # add flag story
-            for i in range(len(story_embeds[batch])):
-                x_new = torch.cat((story_embeds[batch][i], self.flag_story), 0)
-                story_vector[batch, i, :] = x_new
-    
-            # add flag ending1
-            for i in range(len(option1_embeds[batch])):
-                x_new = torch.cat((option1_embeds[batch][i], self.flag_o1), 0)
-                o1_vector[batch, i, :] = x_new
-    
-            # add flag ending2
-            for i in range(len(option2_embeds[batch])):
-                x_new = torch.cat((option2_embeds[batch][i], self.flag_o2), 0)
-                o2_vector[batch, i, :] = x_new
-    
-        query = torch.zeros((batch_size, 1, self._word_size)).to(self.device)
-    
-        # add query
+        # #add query
+        query = self.query.repeat(batch_size,1,1)
         o1_vector = torch.cat((o1_vector, query), 1)
         o2_vector = torch.cat((o2_vector, query), 1)
-    
+
         # feed the data
         initial_state = self.dnc.initial_state(batch_size)
         story_vector, story_state, hr_story, hw_story = self.dnc(story_vector, initial_state)
